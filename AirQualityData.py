a914b3a38cc63b39f21084877f2a6d405c6d7f32
Kamil <kam.popczyk@gmail.com>
2019-01-10 13:55:16 +0100

    Search stations in city

--- AirQualityData.py
+++ AirQualityData.py
@@ -1,5 +1,7 @@
 import json
 import urllib.request
+import string
+
 
 class AirQualityData():
     @staticmethod
@@ -13,4 +15,17 @@ class AirQualityData():
             stations_dict[station['stationName']] = station['id']
 
         return stations_dict
+
+    @staticmethod
+    def find_stations_city(city_name: string):
+        url = 'http://api.gios.gov.pl/pjp-api/rest/station/findAll'
+        with urllib.request.urlopen(url) as url:
+            stations_list = json.loads(url.read().decode())
+
+        city_stations_dict = {}
+        for station in stations_list:
+           if station['city']['name'] == city_name:
+               city_stations_dict[station['stationName']] = station['id']
+
+        return city_stations_dict
 
--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from AirQualityData import *
 
 
 if __name__ == "__main__":
-    AirQualityData.show_station()
+    AirQualityData.give_all_stations()
+    AirQualityData.find_stations_city('Wrocław')
